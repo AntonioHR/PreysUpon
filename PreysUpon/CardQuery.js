d766cd940ed0b173dce0bf10b10926edaecc2c92
Antonio Harger Rivero <antonio.rivero@gprt.ufpe.br>
2017-06-21 14:22:30 -0300

    Tabs and Spacing fixes Foi mal. O TOC falou mais alto

--- PreysUpon/CardQuery.js
+++ PreysUpon/CardQuery.js
@@ -6,56 +6,56 @@ var CardQuery = (function(Cards)
 
 
 	function onlyUniqueFilter(value, index, self) {
-	    return self.indexOf(value) === index;
+		return self.indexOf(value) === index;
 	}
 	function anyMatches(array1, array2)
 	{
-	    for (var a in array1) {
-	        if (array2.indexOf(array1[a]) != -1)
-	            return true;
-	    }
-	    return false;
+		for (var a in array1) {
+			if (array2.indexOf(array1[a]) != -1)
+				return true;
+		}
+		return false;
 	}
 	function hasNoColors(card)
 	{
-	    return card.colors === undefined;
+		return card.colors === undefined;
 	}
 
 
 
 	function getCardWithName(name) {
-	    var card = Cards.find(function (card) {
-	        return card.name == name;
-	    });
-	    return card;
+		var card = Cards.find(function (card) {
+			return card.name == name;
+		});
+		return card;
 	}
 	//Helper filter functions
 	function getColorGroupSplit()
 	{
-	    var result =
-	        {
-	            Colorless: getAllColorless(),
-	            Multicolored: getAllMulticolored()
-	        };
-	    for (var c in AllColors)
-	    {
-	        result[AllColors[c]] = getAllWithColorMono(AllColors[c]);
-	    }
-	    return result;
+		var result =
+			{
+				Colorless: getAllColorless(),
+				Multicolored: getAllMulticolored()
+			};
+		for (var c in AllColors)
+		{
+			result[AllColors[c]] = getAllWithColorMono(AllColors[c]);
+		}
+		return result;
 	}
 
 	function getColorPredationSplit(pow, tough)
 	{
-	    var result =
-	        {
-	            Colorless: getAllColorless().getPredationSplit(pow, tough),
-	            Multicolored: getAllMulticolored().getPredationSplit(pow, tough)
-	        };
-	    for (var c in AllColors)
-	    {
-	        result[AllColors[c]] = getAllWithColorMono(AllColors[c]).getPredationSplit(pow, tough);
-	    }
-	    return result;
+		var result =
+			{
+				Colorless: getAllColorless().getPredationSplit(pow, tough),
+				Multicolored: getAllMulticolored().getPredationSplit(pow, tough)
+			};
+		for (var c in AllColors)
+		{
+			result[AllColors[c]] = getAllWithColorMono(AllColors[c]).getPredationSplit(pow, tough);
+		}
+		return result;
 	}
 
 
@@ -70,13 +70,13 @@ var CardQuery = (function(Cards)
 		Multicolored.key  = "Multicolored";
 		result.push(Multicolored);
 
-	    for (var c in AllColors)
-	    {
-	    	var thisColor = getAllWithColorMono(AllColors[c]).getPredationSplit(pow, tough);
-	    	thisColor.key = AllColors[c];
-	        result.push(thisColor);
-	    }
-	    return result;
+		for (var c in AllColors)
+		{
+			var thisColor = getAllWithColorMono(AllColors[c]).getPredationSplit(pow, tough);
+			thisColor.key = AllColors[c];
+			result.push(thisColor);
+		}
+		return result;
 	}
 
 	function getPredationColorSplitTable(pow, tough)
@@ -98,75 +98,75 @@ var CardQuery = (function(Cards)
 		var Predator =  getAllPredators(pow, tough).getColorGroupSplit();
 		Predator.key  = "Predator";
 		result.push(Predator);
-	    return result;
+		return result;
 	}
 
 
 	function getPredationSplit(pow, tough)
 	{
 		var result =
-	        {
-        		Prey:getAllPrey(pow, tough),
-        		BounceOff:getAllBounceOff(pow, tough),
-        		Trade:getAllTrades(pow, tough),
-        		Predator:getAllPredators(pow, tough),
-        		cardCount:size
-	        };
-        return result;
+			{
+				Prey:getAllPrey(pow, tough),
+				BounceOff:getAllBounceOff(pow, tough),
+				Trade:getAllTrades(pow, tough),
+				Predator:getAllPredators(pow, tough),
+				cardCount:size
+			};
+		return result;
 	}
 
 	function getAllColorless() {
-	    return CardQuery(Cards.filter(hasNoColors));
+		return CardQuery(Cards.filter(hasNoColors));
 	}
 	function getAllMulticolored() {
-	    var filter = Cards.filter(function (card) {
-	        return !hasNoColors(card) && card.colors.length > 1;
-	    });
-	    return CardQuery(filter);
+		var filter = Cards.filter(function (card) {
+			return !hasNoColors(card) && card.colors.length > 1;
+		});
+		return CardQuery(filter);
 	}
 	function getAllWithColorMono(color)
 	{
-	    var filter = Cards.filter(function (card) {
-	        return !hasNoColors(card) && card.colors.length == 1 && card.colors.indexOf(color) != -1;
-	    });
-	    return CardQuery(filter);
+		var filter = Cards.filter(function (card) {
+			return !hasNoColors(card) && card.colors.length == 1 && card.colors.indexOf(color) != -1;
+		});
+		return CardQuery(filter);
 	}
 	function getAllInEdition(editions)
 	{
-	    var filter = Cards.filter(function (card) {
-	        return anyMatches(card.printings, editions);
-	    });
-	    return CardQuery(filter);
+		var filter = Cards.filter(function (card) {
+			return anyMatches(card.printings, editions);
+		});
+		return CardQuery(filter);
 	}
 
 	//When combat results in no deaths
 	function getAllBounceOff(power, toughness) {
-	    var filter =  Cards.filter(function (card) {
-	        return card.power < toughness && card.toughness > power;
-	    });
-	    return CardQuery(filter);
+		var filter =  Cards.filter(function (card) {
+			return card.power < toughness && card.toughness > power;
+		});
+		return CardQuery(filter);
 	}
 
 	//When combat results both dying
 	function getAllTrades(power, toughness) {
-	    var filter =  Cards.filter(function (card) {
-	        return card.power >= toughness && card.toughness <= power;
-	    });
-	    return CardQuery(filter);
+		var filter =  Cards.filter(function (card) {
+			return card.power >= toughness && card.toughness <= power;
+		});
+		return CardQuery(filter);
 	}
 	function getAllPrey(power, toughness)
 	{
-	    var filter = Cards.filter(function(card)
-	    {
-	        return card.power < toughness && card.toughness <= power;
-	    });
-	    return CardQuery(filter);
+		var filter = Cards.filter(function(card)
+		{
+			return card.power < toughness && card.toughness <= power;
+		});
+		return CardQuery(filter);
 	}
 	function getAllPredators(power, toughness) {
-	    var filter = Cards.filter(function (card) {
-	        return card.power >= toughness && card.toughness > power;
-	    });
-	    return CardQuery(filter);
+		var filter = Cards.filter(function (card) {
+			return card.power >= toughness && card.toughness > power;
+		});
+		return CardQuery(filter);
 	}
 	return {
 		cards: Cards,
@@ -188,13 +188,13 @@ var CardQuery = (function(Cards)
 
 function ParseCreaturesFromAllCards(mtg) {
 	var creatures = [];
-    for (var key in mtg) {
-        var card = mtg[key];
-        if (!card.types) continue;
+	for (var key in mtg) {
+		var card = mtg[key];
+		if (!card.types) continue;
 
-        if (card.types.indexOf("Creature") > -1) {
-            creatures.push(card);
-        }
-    }
-    return CardQuery(creatures);
+		if (card.types.indexOf("Creature") > -1) {
+			creatures.push(card);
+		}
+	}
+	return CardQuery(creatures);
 }
--- PreysUpon/stackedPreyHistogram.js
+++ PreysUpon/stackedPreyHistogram.js
@@ -1,54 +1,54 @@
 function makeHisto(svg){
 
-    var margin = {top: 20, right: 20, bottom: 30, left: 40},
-	    width = +svg.attr("width") - margin.left - margin.right,
-	    height = +svg.attr("height") - margin.top - margin.bottom,
-	    g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")"),
-	    colorGroups = ["Blue", "White", "Green",
+	var margin = {top: 20, right: 20, bottom: 30, left: 40},
+		width = +svg.attr("width") - margin.left - margin.right,
+		height = +svg.attr("height") - margin.top - margin.bottom,
+		g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")"),
+		colorGroups = ["Blue", "White", "Green",
 			"Black", "Red", "Multicolored", "Colorless"],
 		predationGroups = ["Prey", "Trade", "BounceOff", "Predator"],
 		rarities = ["Common", "Uncommon", "Rare", "Mythic Rare", "Special"];
 
-    var x = d3.scaleBand()
-    	.rangeRound([0, width])
-    	.paddingInner(0.05)
-    	.align(0.1)
-    	.domain(colorGroups);
+	var x = d3.scaleBand()
+		.rangeRound([0, width])
+		.paddingInner(0.05)
+		.align(0.1)
+		.domain(colorGroups);
 	var y = d3.scaleLinear()
 		.rangeRound([height, 0]);
 
 	var z = d3.scaleOrdinal()
 		.range(["#42f47d", "#ddff47", "#d4ffaa", "#ff7723"])
-    	.domain(predationGroups);
+		.domain(predationGroups);
 
 	var colorsRarity = d3.scaleOrdinal()
 		.range(["#FFFFFF", "#7a7a7a", "#efc323", "#d33d02", "#6e04cc"])
-    	.domain(rarities);
+		.domain(rarities);
 
 
 	var tooltipDiv = d3.select("body").append("div")
-	    .attr("class", "tooltip");
+		.attr("class", "tooltip");
 
 	tooltipDiv.append("ul");
-	    // .style("opacity", 0);
+		// .style("opacity", 0);
 
 	function translate(x, y)
 	{
-	  return "translate("+x+","+y+")";
+		return "translate("+x+","+y+")";
 	}
-	var update = function data(data)
+	var update = function data(newData)
 	{
 
 		var t = d3.transition()
-	      .duration(750);
+			.duration(750);
 
-		y.domain([0, d3.max(data, function(d){return d.cardCount;})]).nice();
+		y.domain([0, d3.max(newData, function(d){return d.cardCount;})]).nice();
 		var dataStacks = d3.stack()
 		.keys(predationGroups)
 		.value(function(d, key)
 			{
 				return d[key].cardCount;
-			})(data);
+			})(newData);
 
 		for(var i in dataStacks)
 		{
@@ -66,20 +66,20 @@ function makeHisto(svg){
 		var newElements = elements.enter().append("g")
 			.attr("class", "bar");
 		elements = newElements.merge(elements)
-	      	.attr("fill", function(d){return z(d.key);})
+			.attr("fill", function(d){return z(d.key);})
 
 
-	  	var subElements = elements.selectAll("rect")
-	  		.data(function(d)
-	  			{
-	  				return d;
-	  			});
+		var subElements = elements.selectAll("rect")
+			.data(function(d)
+				{
+					return d;
+				});
 			subElements.exit().remove();
 		var subNewElements = subElements.enter().append("rect")
 			.attr("y", height)
 			.attr("height", 0)
 			.on("mouseover", onMouseOver)
-	        .on("mouseout", onMouseOut);
+			.on("mouseout", onMouseOut);
 
 		subNewElements.merge(subElements)
 			.attr("x", function(d){return x(d.data.key);})
@@ -89,74 +89,75 @@ function makeHisto(svg){
 			.attr("height", function(d){return y(d[0]) - y(d[1]);});
 
 		var axesParent = g.append("g").attr("id", "axesParent");
-	    axesParent.append("g")
-	      .attr("class", "axis")
-	      .attr("transform", translate(0, height))
-	      .call(d3.axisBottom(x));
-	    axesParent.append("g")
-	        .attr("class", "axis")
-	        .call(d3.axisLeft(y).ticks(null, "s"))
-	      .append("text")
-	        .attr("x", 2)
-	        .attr("y", y(y.ticks().pop()) + 0.5)
-	        .attr("dy", "0.32em")
-	        .attr("fill", "#000")
-	        .attr("font-weight", "bold")
-	        .attr("text-anchor", "start")
-	        .text("Card Count");
+		axesParent.append("g")
+			.attr("class", "axis")
+			.attr("transform", translate(0, height))
+			.call(d3.axisBottom(x));
+
+		axesParent.append("g")
+				.attr("class", "axis")
+				.call(d3.axisLeft(y).ticks(null, "s"))
+			.append("text")
+				.attr("x", 2)
+				.attr("y", y(y.ticks().pop()) + 0.5)
+				.attr("dy", "0.32em")
+				.attr("fill", "#000")
+				.attr("font-weight", "bold")
+				.attr("text-anchor", "start")
+				.text("Card Count");
 
 		var legendParent = g.append("g").attr("id", "axesParent");
-	    var legend = legendParent.append("g")
-	      	.attr("font-family", "sans-serif")
-	        .attr("font-size", 10)
-	        .attr("text-anchor", "end")
-	      .selectAll("g")
-	      .data(predationGroups)
-	      .enter().append("g")
-	        .attr("transform", function(d, i){return translate(0, i*20)});
+		var legend = legendParent.append("g")
+				.attr("font-family", "sans-serif")
+				.attr("font-size", 10)
+				.attr("text-anchor", "end")
+			.selectAll("g")
+			.data(predationGroups)
+			.enter().append("g")
+				.attr("transform", function(d, i){return translate(0, i*20);});
 
-	      legend.append("rect")
-	        .attr("x", width - 19)
-	        .attr("width", 19)
-	        .attr("height", 19)
-	        .attr("fill", z);
+		legend.append("rect")
+			.attr("x", width - 19)
+			.attr("width", 19)
+			.attr("height", 19)
+			.attr("fill", z);
 
-	      legend.append("text")
-	        .attr("x", width - 24)
-	        .attr("y", 9.5)
-	        .attr("dy", "0.32em")
-	        .text(function(d){return d;});
+		legend.append("text")
+			.attr("x", width - 24)
+			.attr("y", 9.5)
+			.attr("dy", "0.32em")
+			.text(function(d){return d;});
 	};
 
 	function onMouseOver(d) {
-	    tooltipDiv.transition()
-	        .duration(200)
-	        .style("opacity", 1)
-	        .style("left", (d3.event.pageX) + "px")
-	        .style("top", (d3.event.pageY - 28) + "px");
+		tooltipDiv.transition()
+			.duration(200)
+			.style("opacity", 1)
+			.style("left", (d3.event.pageX) + "px")
+			.style("top", (d3.event.pageY - 28) + "px");
 
-	    var data = d.data[d.key].cards;
-	    data.sort(function(a, b)
-	    {
-	    	var indexA = rarities.indexOf(a.rarity);
-	    	var indexB = rarities.indexOf(b.rarity);
-	    	return indexB - indexA;
-	    });
-        var elements = tooltipDiv.select("ul").selectAll("li")
-        	.data(data);
+		var data = d.data[d.key].cards;
+		data.sort(function(a, b)
+		{
+			var indexA = rarities.indexOf(a.rarity);
+			var indexB = rarities.indexOf(b.rarity);
+			return indexB - indexA;
+		});
+		var elements = tooltipDiv.select("ul").selectAll("li")
+			.data(data);
 
-        elements.exit().remove();
+		elements.exit().remove();
 
-        var newElements = elements.enter().append("li");
+		var newElements = elements.enter().append("li");
 
-    	elements.merge(newElements)
-    		.text(function(d){return d.cmc + "-" + d.name + "(" + d.power + "/" + d.toughness + ")";})
-    		.style("color", function(d){return colorsRarity(d.rarity);});
+		elements.merge(newElements)
+			.text(function(d){return d.cmc + "-" + d.name + "(" + d.power + "/" + d.toughness + ")";})
+			.style("color", function(d){return colorsRarity(d.rarity);});
 	}
 	function onMouseOut(d) {
-        tooltipDiv.transition()
-            .duration(500)
-            .style("opacity", 0);
+		tooltipDiv.transition()
+			.duration(500)
+			.style("opacity", 0);
 	}
 	return {
 		update:update
